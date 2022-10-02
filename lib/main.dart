5acfa2e8d45487dff2efd16faa312cf879715f8b
Doni Mulya Syahputra <donikuh21@gmail.com>
2022-10-02 14:12:05 +0700

    fix: 🐛 build

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,11 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:restaurant_app/app/app.dart';
 import 'package:restaurant_app/core/core.dart';
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   final NotificationHelper notificationHelper = NotificationHelper();
-  await notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
-  notificationHelper.requestIOSPermissions(flutterLocalNotificationsPlugin);
+  await notificationHelper.initNotifications(FlutterLocalNotificationsPlugin());
 
   runApp(const App());
 }
